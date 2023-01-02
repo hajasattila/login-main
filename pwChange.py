85ad9fa65bae3cb02a4d9709da6cdb731e5f36cf
Hajas Attila <hajaattila@gmail.com>
2023-01-02 17:55:31 +0100

    pwChange first rules

--- pwChange.py
+++ pwChange.py
@@ -3,12 +3,37 @@ import tkinter as tk
 import os
 import hashlib
 
+
+#-------------------------------------------------Baisc settings----------------------------------------------------------------
+
+
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 root = customtkinter.CTk()
 root.geometry("500x350")
 root.title("Password change")
 root.resizable(False, False)
+
+#-------------------------------------------------functions----------------------------------------------------------------
+
+
+def pwChange(*event):
+    # Get the user input
+    username = entry1.get()
+    oldPassword = entry2.get()
+    newPassword = entry3.get()
+    newPasswordagain = entry4.get()
+    
+    if newPassword != newPasswordagain:
+        tk.messagebox.showerror(
+            "Hiba!", "Nem egyeznek a jelszavak!")
+        return
+    
+    if not username or not oldPassword or not newPasswordagain or not newPasswordagain:
+        tk.messagebox.showerror(
+            "Hiba!", "Ne hagyj üresen mezőt!!")
+        return
+#-------------------------------------------------Frontend----------------------------------------------------------------
 
 
 frame = customtkinter.CTkFrame(master=root)
@@ -29,9 +54,9 @@ entry3.pack(pady=12, padx=10)
 entry4 = customtkinter.CTkEntry(master=frame, placeholder_text="Új jelszó mégegyszer", show="*")
 entry4.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Megerősítés!")
+button = customtkinter.CTkButton(master=frame, text="Megerősítés!", command=lambda: pwChange)
 button.pack(pady=5, padx=100)
-button.bind("<Button-1>")
+button.bind("<Button-1>", pwChange)
 
 
 root.mainloop()
