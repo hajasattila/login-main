8c5d666c0156054248ce57507e5f399671ddafab
Hajas Attila <hajaattila@gmail.com>
2023-01-07 01:57:06 +0100

    pwReminder actaully send an e-mail

--- pwReminder.py
+++ pwReminder.py
@@ -1,6 +1,9 @@
 import customtkinter
 import tkinter as tk
 from tkinter import *
+import smtplib
+from email.mime.text import MIMEText
+import hashlib
 
 #-------------------------------------------------Alap beállítások----------------------------------------------------------------
 
@@ -17,21 +20,59 @@ root.iconbitmap(r'images/favicon.ico')
 
 #-------------------------------------------------Function----------------------------------------------------------------
 
-
+#basic check stuff
 def check_entries():
     name = entry1.get()
     email = entry2.get()
+    
+    with open("smtp.txt", "r") as f:
+        hashed_password = f.read().strip()
+        
 
     if not name or not email:
         tk.messagebox.showerror("Hiba!", "Töltsd ki az össze mezőt!")
         return
-    print("All the entries are filled out.")
+    
+    try:
+        with open("users.txt", "r") as f:
+            registered_users = f.read()
+            if name not in registered_users:
+                tk.messagebox.showerror(
+                    "Hiba!", "Nem szerepel a nyilvántartásban ez a felhasználónév!")
+                return
+    except FileNotFoundError:
+        pass
+        
+        
+    if '@' not in email or (['.com', '.hu', '.org', '.net', '.biz', '.co'] not in email):
+        tk.messagebox.showerror("Hiba!", "Kérlek valós e-mail címet írj be!")
+        return
+    
+    print("minden valid.")
+    
+#-------------------------------------------------email sending----------------------------------------------------------------
     
     
-#-------------------------------------------------PW megmutatása----------------------------------------------------------------
+    smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+    smtp_ssl_port = 465
+    username = 'hajasatlasz@gmail.com'
+    password = hashed_password
+    sender = 'hajasatlasz@gmail.com'
+    targets = entry2.get()
 
+    msg = MIMEText('Megérkezett az új jelszavad! Belépésnél meg tudod majd változtatni!')
+    msg['Subject'] = 'Jelszó emlékeztető'
+    msg['From'] = sender
+    msg['To'] = ', '.join(targets)
 
-
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server.login(username, password)
+    server.sendmail(sender, targets, msg.as_string())
+    server.quit()
+    
+    tk.messagebox.showinfo(
+        "Siker!", "Elküldtök az e-mailt!\nPerceken belül meg fog érkezni!")
+    root.destroy()
 
 
 #-------------------------------------------------Frontend----------------------------------------------------------------
@@ -51,7 +92,7 @@ entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="E-mail cím")
 entry2.pack(pady=18, padx=10)
 
 customtkinter.set_default_color_theme("blue")
-button = customtkinter.CTkButton(master=frame, text="Kérem az új jelszót!", command=check_entries)
+button = customtkinter.CTkButton(master=frame, text="Kérem az új jelszót!", command= check_entries)
 button.pack(pady=18, padx=10)
 
 label = customtkinter.CTkLabel(
